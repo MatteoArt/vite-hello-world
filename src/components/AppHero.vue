8ba0031ce1075ebdecb24fb5de0a4f843a3e7172
MatteoArt <mattrim8@gmail.com>
2023-08-21 18:27:56 +0200

    termine esercizio (incluso bonus)

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+    <img src="../assets/hero_banner.webp" alt="img">
+</template>
+
+<style>
+img {
+    width: 100%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+h1 {
+  color: goldenrod;
+  text-align: center;
+}
+</style>
